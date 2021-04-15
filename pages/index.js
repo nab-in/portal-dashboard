7ac0667d03f15a507635eb8303fea1540bdc7f9f
Benedict <bmsteve96@gmail.com>
2021-04-16 02:09:30 +0300

    initial commit

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import Head from "next/head"
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Portal Dashboard</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <h1>Portal dashboard</h1>
+    </div>
+  )
+}
