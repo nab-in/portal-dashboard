fa879700a96c6ec55c69817dc8fb58c69af52670
Benedict <bmsteve96@gmail.com>
2021-04-19 00:34:34 +0300

    added sample data

---
+++ data/categories.js
@@ -0,0 +1,58 @@
+const categories = [
+  {
+    id: 1,
+    name: "Class",
+    sub_categories: [
+      {
+        id: 1,
+        name: "Class A",
+      },
+      {
+        id: 2,
+        name: "Class B",
+      },
+      {
+        id: 3,
+        name: "Class C",
+      },
+    ],
+  },
+  {
+    id: 2,
+    name: "Type",
+    sub_categories: [
+      {
+        id: 3,
+        name: "Type A",
+      },
+      {
+        id: 6,
+        name: "Type B",
+      },
+      {
+        id: 15,
+        name: "Type C",
+      },
+    ],
+  },
+  {
+    id: 27,
+    name: "Open To",
+    sub_categories: [
+      {
+        id: 188,
+        name: "Consultants",
+      },
+      {
+        id: 28,
+        name: "Software Developers",
+      },
+      {
+        id: 48,
+        name: "Water Engineers",
+      },
+    ],
+  },
+]
+
+export default categories
