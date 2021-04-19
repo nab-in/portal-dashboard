21a5b348bd18b6755fdb2f1d2524eff7cde85431
Benedict <bmsteve96@gmail.com>
2021-04-19 14:40:42 +0300

    added sample chart data and fixed tooltip date bug

--- components/chart/Chart.js
+++ components/chart/Chart.js
@@ -10,114 +10,13 @@ import {
   Legend,
   ResponsiveContainer,
 } from "recharts"
+import data from "../../data/chart.js"
+import moment from "moment"
 import styles from "./chart.module.sass"
 
-const data = [
-  {
-    name: "Page A",
-    uv: 4000,
-    pv: 2400,
-    amt: 2400,
-  },
-  {
-    name: "Page B",
-    uv: 3000,
-    pv: 1398,
-    amt: 2210,
-  },
-  {
-    name: "Page C",
-    uv: 2000,
-    pv: 9800,
-    amt: 2290,
-  },
-  {
-    name: "Page D",
-    uv: 2780,
-    pv: 3908,
-    amt: 2000,
-  },
-  {
-    name: "Page E",
-    uv: 1890,
-    pv: 4800,
-    amt: 2181,
-  },
-  {
-    name: "Page F",
-    uv: 2390,
-    pv: 3800,
-    amt: 2500,
-  },
-  {
-    name: "Page G",
-    uv: 3490,
-    pv: 4300,
-    amt: 2100,
-  },
-  {
-    name: "Page F",
-    uv: 2390,
-    pv: 3800,
-    amt: 2500,
-  },
-  {
-    name: "Page G",
-    uv: 3490,
-    pv: 4300,
-    amt: 2100,
-  },
-  {
-    name: "Page F",
-    uv: 2390,
-    pv: 3800,
-    amt: 2500,
-  },
-  {
-    name: "Page G",
-    uv: 3490,
-    pv: 4300,
-    amt: 2100,
-  },
-  {
-    name: "Page F",
-    uv: 2390,
-    pv: 3800,
-    amt: 2500,
-  },
-  {
-    name: "Page G",
-    uv: 3490,
-    pv: 4300,
-    amt: 2100,
-  },
-  {
-    name: "Page F",
-    uv: 2390,
-    pv: 3800,
-    amt: 2500,
-  },
-  {
-    name: "Page G",
-    uv: 3490,
-    pv: 4300,
-    amt: 2100,
-  },
-  {
-    name: "Page F",
-    uv: 2390,
-    pv: 3800,
-    amt: 2500,
-  },
-  {
-    name: "Page G",
-    uv: 3490,
-    pv: 4300,
-    amt: 2100,
-  },
-]
-
 const CustomTooltip = ({ active, payload, label }) => {
+  let date = payload[0]?.payload.date
+  console.log(date);
   if (active && payload && payload.length) {
     return (
       <div
@@ -129,7 +28,7 @@ const CustomTooltip = ({ active, payload, label }) => {
           boxShadow: "2px 4px 10px 3px rgba(0, 0, 0, 0.15)",
         }}
       >
-        <p className={styles.label}>{`${label} : ${payload[0].value}`}</p>
+        <p className={styles.label}>{moment(date, "DD/MM/YYYY").format("dddd MMMM D, yyyy")} <br /> {payload[0].value} Applicants</p>
       </div>
     )
   }
@@ -141,36 +40,31 @@ let Subtitle = () => {
   return <div className="badge">Filter By</div>
 }
 
-let Text = ({ text }) => <text style={{ marginTop: "2rem" }}>{text}</text>
-
 const Chart = ({ title }) => {
   return (
     <div className={styles.chart}>
       <Card title={title}>
       <Subtitle />
       <div className={styles.chart__display}>
-        <ResponsiveContainer width={800} minWidth="100%" height={400}>
           <BarChart
-            width="100%"
+            width={700}
             height={300}
             data={data}
             margin={{
-              top: 5,
+              top: 10,
               right: 30,
-              left: 20,
-              bottom: 5,
+              left: 0,
+              bottom: 10,
             }}
           >
-            <CartesianGrid strokeDasharray="3 3" />
+            <CartesianGrid strokeDasharray="0 0" />
             <XAxis
               dataKey="name"
-              label={<Text text="Applications/Day" />}
             />
-            <YAxis label="Text" />
+            <YAxis  />
             <Tooltip content={<CustomTooltip />} />
-            <Bar dataKey="pv" barSize={20} fill="#007CA9" />
+            <Bar dataKey="value" label="date" barSize={15} fill="#007CA9" />
           </BarChart>
-        </ResponsiveContainer>
         </div>
       </Card>
     </div>
