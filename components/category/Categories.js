38107eae0877f556f4d2903d8b3ac9514b73e214
Benedict <bmsteve96@gmail.com>
2021-07-30 00:37:02 +0300

    add working accordion

---
+++ components/category/Categories.js
@@ -0,0 +1,12 @@
+import React from "react"
+import Accordion from "../accordion/Accordion"
+
+const Categories = ({ categories, setcategories }) => {
+  return (
+    <div>
+      <Accordion title="Title">"Hello world!!"</Accordion>
+    </div>
+  )
+}
+
+export default Categories
--- pages/categories.js
+++ pages/categories.js
@@ -5,6 +5,7 @@ import Link from "next/link"
 import MainContents from "../components/templates/MainContents"
 import SubContents from "../components/templates/SubContents"
 import Filter from "../components/categoryFilters/Filter"
+import Categories from "../components/category/Categories"
 
 const categories = () => {
   let [categories, setcategories] = useState([])
@@ -39,6 +40,7 @@ const categories = () => {
             <span>/</span>
             <span>Categories</span>
           </div>
+          <Categories categories={categories} setcategories={setcategories} />
         </MainContents>
         <SubContents>
           <Filter categories={categories} setcategories={setcategories} />
