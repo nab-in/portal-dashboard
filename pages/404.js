3cfab96222f34c04f102d8e290aadd5eceb0e18a
Benedict <bmsteve96@gmail.com>
2021-07-30 17:00:21 +0300

    finish companies page UI

--- pages/404.js
+++ pages/404.js
@@ -1,23 +1,18 @@
 import Error from "../components/error/Error"
 import Link from "next/link"
-import MainContents from "../components/templates/MainContents"
-import SubContents from "../components/templates/SubContents"
 
 export default function Custom404() {
   return (
     <div>
       <div className="content">
-        <MainContents>
-          <div className="bread__crumb">
-            <span>
-              <Link href="/">Home</Link>
-            </span>
-            <span>/</span>
-            <span>404 Page not found</span>
-          </div>
-          <Error />
-        </MainContents>
-        <SubContents></SubContents>
+        <div className="bread__crumb">
+          <span>
+            <Link href="/">Home</Link>
+          </span>
+          <span>/</span>
+          <span>404 Page not found</span>
+        </div>
+        <Error />
       </div>
     </div>
   )
