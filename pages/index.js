a1bf83a12fa7d82d77d318adac25116de44474bf
Benedict <bmsteve96@gmail.com>
2021-08-07 13:07:57 +0300

    add condition render to home page

--- pages/index.js
+++ pages/index.js
@@ -6,9 +6,11 @@ import SubContents from "../components/templates/SubContents"
 import MetricsCard from "../components/metricsCard/MetricsCard"
 import RecentJobs from "../components/jobs/RecentJobs"
 import RecentUsers from "../components/user/RecentUsers"
+import { useAuthState } from "../context/auth"
 const Chart = dynamic(() => import("../components/chart/Chart"), { ssr: false })
 
 export default function Home() {
+  const { user } = useAuthState()
   return (
     <div>
       <Head>
@@ -21,11 +23,13 @@ export default function Home() {
             <span>Home</span>
             <span>/</span>
           </div>
-          <div className="mobile__link">
-            <Link href="/jobs/new_job">
-              <a>Add New Job</a>
-            </Link>
-          </div>
+          {user && user?.identity?.name == "company" && (
+            <div className="mobile__link">
+              <Link href="/jobs/new_job">
+                <a>Add New Job</a>
+              </Link>
+            </div>
+          )}
           <div className="mt-1">
             <div className="metrics__showcase">
               <MetricsCard title="Total Number of Jobs" number={88} />
@@ -33,13 +37,21 @@ export default function Home() {
               <MetricsCard title="Total Number of Jobs" number={88} />
               <MetricsCard title="Total Number of Jobs" number={1190088} />
             </div>
-            <Chart title="Applications" />
+            <Chart title="Job Applicants" />
+            {user && user?.identity?.name == "admin" && (
+              <>
+                <Chart title="Companies Registered" />
+                <Chart title="Users Registered" />
+              </>
+            )}
           </div>
         </MainContents>
         <SubContents>
-          <Link href="/jobs/new_job">
-            <a className="sub_btn btn btn-primary">Add New Job</a>
-          </Link>
+          {user && user?.identity?.name == "company" && (
+            <Link href="/jobs/new_job">
+              <a className="sub_btn btn btn-primary">Add New Job</a>
+            </Link>
+          )}
           <RecentJobs />
           <RecentUsers size={3} />
         </SubContents>
