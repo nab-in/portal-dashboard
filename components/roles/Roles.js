70926c33d02b167f413850a853d849c2ffd86b46
Benedict <bmsteve96@gmail.com>
2021-08-25 18:50:01 +0300

    remove add role

--- components/roles/Roles.js
+++ components/roles/Roles.js
@@ -11,51 +11,51 @@ import { useAlertsDispatch } from "../../context/alerts"
 import { useAuthDispatch, useAuthState } from "../../context/auth"
 
 const Roles = () => {
-  const [formData, setFormData] = useState({
-    name: "",
-  })
-  const [loading, setLoading] = useState(true)
-  const [addLoading, setAddLoading] = useState(false)
+  // const [formData, setFormData] = useState({
+  //   name: "",
+  // })
+  // const [loading, setLoading] = useState(true)
+  // const [addLoading, setAddLoading] = useState(false)
   const dispatch = useAuthDispatch()
-  const alertsDispatch = useAlertsDispatch()
+  // const alertsDispatch = useAlertsDispatch()
   const { roles } = useAuthState()
-  const { name } = formData
-  const handleChange = (e) => {
-    setFormData({ name: e.target.value })
-  }
-  const handleSubmit = (e) => {
-    e.preventDefault()
-    const token = Cookies.get("token")
-    const config = {
-      headers: {
-        authorization: `Bearer ` + token,
-      },
-    }
-    setAddLoading(true)
-    axios
-      .post(`${API}/userRoles`, formData, config)
-      .then((res) => {
-        dispatch({
-          type: "ADD_ROLE",
-          payload: res.data.payload,
-        })
-        alertsDispatch({
-          type: "ADD",
-          payload: {
-            message: res.data.message,
-            type: "success",
-          },
-        })
-        setAddLoading(false)
-        setFormData({
-          name: "",
-        })
-      })
-      .catch((err) => {
-        console.log(err)
-        setAddLoading(false)
-      })
-  }
+  // const { name } = formData
+  // const handleChange = (e) => {
+  //   setFormData({ name: e.target.value })
+  // }
+  // const handleSubmit = (e) => {
+  //   e.preventDefault()
+  //   const token = Cookies.get("token")
+  //   const config = {
+  //     headers: {
+  //       authorization: `Bearer ` + token,
+  //     },
+  //   }
+  //   setAddLoading(true)
+  //   axios
+  //     .post(`${API}/userRoles`, formData, config)
+  //     .then((res) => {
+  //       dispatch({
+  //         type: "ADD_ROLE",
+  //         payload: res.data.payload,
+  //       })
+  //       alertsDispatch({
+  //         type: "ADD",
+  //         payload: {
+  //           message: res.data.message,
+  //           type: "success",
+  //         },
+  //       })
+  //       setAddLoading(false)
+  //       setFormData({
+  //         name: "",
+  //       })
+  //     })
+  //     .catch((err) => {
+  //       console.log(err)
+  //       setAddLoading(false)
+  //     })
+  // }
   useEffect(() => {
     const token = Cookies.get("token")
     const config = {
@@ -82,7 +82,7 @@ const Roles = () => {
       <h2>Admin Roles</h2>
       {roles?.length > 0 &&
         roles.map((role) => <Role key={role.id} role={role} />)}
-      <form onSubmit={(e) => handleSubmit(e)} className={styles.form}>
+      {/* <form onSubmit={(e) => handleSubmit(e)} className={styles.form}>
         <Input
           handleChange={(e) => handleChange(e)}
           placeholder="New role"
@@ -96,7 +96,7 @@ const Roles = () => {
         >
           {addLoading ? <Loader /> : "Add"}
         </button>
-      </form>
+      </form> */}
     </div>
   )
 }
