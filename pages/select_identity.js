8ce64b6e0fd2aee427e6df81575b19ae3764736e
Benedict <bmsteve96@gmail.com>
2021-08-25 18:18:06 +0300

    update select page

--- pages/select_identity.js
+++ pages/select_identity.js
@@ -24,8 +24,14 @@ const select_identity = () => {
     })
     if (id && name) router.push("/")
   }
+
   useEffect(() => {
-    if (!isAuthenticated && !loading) router.push("/login")
+    if (!isAuthenticated && !loading && !user) {
+      console.log("here")
+      router.push("/login")
+    }
+  }, [loading])
+  useEffect(() => {
     let token = Cookies.get("token")
     let config = {
       headers: {
@@ -53,71 +59,73 @@ const select_identity = () => {
         <></>
       ) : (
         <>
-          <div className={styles.select}>
-            <section>
-              <div className={styles.dp}>
-                <img src={user?.dp} alt="" />
-              </div>
-              <p>Welcome {user?.username}</p>
-              <h1>Please select dashboard to view</h1>
-              {companies?.length > 0 && (
-                <>
-                  <h2>Your Companies</h2>
-                  {companyLoading}
-                  <div className={styles.showcase}>
-                    {companies.map(({ id, name }) => (
-                      <div
-                        key={id}
-                        className={`card ${styles.card}`}
-                        onClick={() => select(id, "company", "")}
-                      >
-                        <div className={styles.company}>
-                          <div className={styles.logo}>
-                            <img
-                              src={`/assets/companies/logo1.png`}
-                              loading="lazy"
-                            />
+          {isAuthenticated && user && (
+            <div className={styles.select}>
+              <section>
+                <div className={styles.dp}>
+                  <img src={user?.dp} alt="" />
+                </div>
+                <p>Welcome {user?.username}</p>
+                <h1>Please select dashboard to view</h1>
+                {companies?.length > 0 && (
+                  <>
+                    <h2>Your Companies</h2>
+                    {companyLoading}
+                    <div className={styles.showcase}>
+                      {companies.map(({ id, name }) => (
+                        <div
+                          key={id}
+                          className={`card ${styles.card}`}
+                          onClick={() => select(id, "company", "")}
+                        >
+                          <div className={styles.company}>
+                            <div className={styles.logo}>
+                              <img
+                                src={`/assets/companies/logo1.png`}
+                                loading="lazy"
+                              />
+                            </div>
+                            <div className={styles.name}>{name}</div>
                           </div>
-                          <div className={styles.name}>{name}</div>
+                          <div className={styles.details}>
+                            <button>Select</button>
+                          </div>
                         </div>
-                        <div className={styles.details}>
-                          <button>Select</button>
-                        </div>
-                      </div>
-                    ))}
-                  </div>
-                </>
-              )}
+                      ))}
+                    </div>
+                  </>
+                )}
 
-              {roles?.length > 0 && (
-                <>
-                  <h2>An Admin?</h2>
-                  <div className={styles.showcase}>
-                    {roles.map(({ id, name }) => (
-                      <div
-                        className={`card ${styles.card}`}
-                        onClick={() => select(id, "admin", name)}
-                        key={id}
-                      >
-                        <div className={styles.name}>{name}</div>
-                        <div className={styles.details}>
-                          <button>Select</button>
+                {roles?.length > 0 && (
+                  <>
+                    <h2>An Admin?</h2>
+                    <div className={styles.showcase}>
+                      {roles.map(({ id, name }) => (
+                        <div
+                          className={`card ${styles.card}`}
+                          onClick={() => select(id, "admin", name)}
+                          key={id}
+                        >
+                          <div className={styles.name}>{name}</div>
+                          <div className={styles.details}>
+                            <button>Select</button>
+                          </div>
                         </div>
-                      </div>
-                    ))}
-                  </div>
-                </>
-              )}
-              {roles?.length === 0 && companies?.length === 0 && (
-                <>
-                  <h2>We could't find any company which you belong</h2>
-                  <p>
-                    <a href="http://localhost:3000">Visit home</a>
-                  </p>
-                </>
-              )}
-            </section>
-          </div>
+                      ))}
+                    </div>
+                  </>
+                )}
+                {roles?.length === 0 && companies?.length === 0 && (
+                  <>
+                    <h2>We could't find any company which you belong</h2>
+                    <p>
+                      <a href="http://localhost:3000">Visit home</a>
+                    </p>
+                  </>
+                )}
+              </section>
+            </div>
+          )}
         </>
       )}
     </>
