b5023777418777e3d1667171d5f1c7a0cd809243
Benedict <bmsteve96@gmail.com>
2021-09-02 15:28:03 +0300

    fix active link error

--- components/ActiveLink.js
+++ components/ActiveLink.js
@@ -1,11 +1,11 @@
-import { Children } from "react"
+import { cloneElement, Children } from "react"
 import { withRouter } from "next/router"
 import Link from "next/link"
 import styles from "./aside/aside.module.sass"
 
 const ActiveLink = withRouter(({ router, children, ...props }) => (
   <Link {...props}>
-    {React.cloneElement(Children.only(children), {
+    {cloneElement(Children.only(children), {
       className:
         `/${router.pathname.split("/")[1]}` === props.href
           ? `${styles.active}`
